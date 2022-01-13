5bd053f181609f57b4487fcec67dc4697c96bb9c
Andrew Yu <andrewyu209@gmail.com>
2022-01-12 20:30:25 -0500

    Added login logic to re-direct to login handlebars page

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -95,7 +95,6 @@ router.post('/login', (req, res) => {
             req.session.user_id = dbUserData.id;
             req.session.email = dbUserData.email;
             req.session.loggedIn = true;
-
             res.json({ user: dbUserData, message: 'You are now logged in!' });
         });
     });
