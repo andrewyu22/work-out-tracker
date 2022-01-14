6fa50a6cbe1e83101fd637d1aff0dc6bfe014c05
John Harris <john.harris7292@gmail.com>
2022-01-14 12:07:15 -0500

    added function

--- public/stylesheets/exercise.js
+++ public/stylesheets/exercise.js
@@ -128,4 +128,14 @@ function handleToastAnimationEnd() {
     }
 }
 
+function clearInputs() {
+    cardioNameInput.value = "";
+    nameInput.value = "";
+    setsInput.value = "";
+    distanceInput.value = "";
+    durationInput.value = "";
+    repsInput.value = "";
+    resistanceDurationInput.value = "";
+    weightInput.value = "";
+}
 
