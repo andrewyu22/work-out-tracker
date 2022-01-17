42e15669c03ee67c453241ee8a6cf9a6028b574b
Marcio R <Murcielago203@gmail.com>
2022-01-16 21:37:14 -0500

    updated error home-route.js

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {loggedIn: req.body.loggedIn});
+    res.render('homepage', {loggedIn: req.session.loggedIn});
 })
 
 router.get('/login', (req, res) => {
