71e37f99839f2937e890dc0b6166225bd98b13e2
Fayaz <flalani100@gmail.com>
2022-01-16 20:02:26 -0500

    Signup handlebar and js updates

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/connection');
 router.get('/', (req, res) => {
     res.render('homepage');
 })
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
@@ -11,5 +12,9 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+})
 
 module.exports = router;
