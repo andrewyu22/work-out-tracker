eb96069b7ee9e7c20ef7eb41821fa5a31a51e77d
Andrew Yu <andrewyu209@gmail.com>
2022-01-19 11:53:29 -0500

    Added exercise route to homepage to get data by date and user_id

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const Exercise = require('../models/Exercise');
 
 router.get('/', (req, res) => {
     res.render('homepage', { loggedIn: req.session.loggedIn });
-})
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
@@ -15,18 +16,40 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
     res.render('signup');
-})
+});
 
 router.get('/about', (req, res) => {
     res.render('about', { loggedIn: req.session.loggedIn });
-})
+});
 
 router.get('/gym', (req, res) => {
     res.render('nearbygym', { loggedIn: req.session.loggedIn });
-})
+});
 
 router.get('/workout', (req, res) => {
     res.render('workout', { loggedIn: req.session.loggedIn });
+});
+
+router.post('/exercise', withAuth, (req, res) => {
+    //get all exercise basis off user & date
+    Exercise.findAll({
+            where: {
+                user_id: req.session.user_id,
+                date: req.body.date
+            }
+        })
+        .then(dbData => {
+            const activity = dbData.map(data => data.get({ plain: true }));
+            res.render('workout', {
+                activity,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
+
 
 module.exports = router;
